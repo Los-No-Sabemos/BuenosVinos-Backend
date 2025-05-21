ec3cc974b83fee8cea1da3b517c8f61991814c2f
Eliana Depietri <eliana.depietri@gmail.com>
2025-05-21 18:21:52 +0200

    add cors

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ require("./db");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
